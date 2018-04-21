26df3a06b29949673189c6fe0e02bef45920f111
Ramana Nagasamudram <rnagasam@stevens.edu>
2018-04-21 17:09:14 -0400

    Working with the AT&T faces dataset

--- faces1.py
+++ faces1.py
@@ -5,12 +5,12 @@ import PIL.ImageOps
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Input, Lambda, Conv2D, Flatten, MaxPooling2D
+from keras.layers import Dense, Dropout, Input, Lambda, Conv2D, Flatten, MaxPooling2D, merge
 from keras.optimizers import Adam, RMSprop, SGD
 from keras.losses import binary_crossentropy
 from keras.regularizers import l2
 from keras import backend as K
-from process_faces import SiameseLoader
+from process_faces import SiameseLoader, DataLoader, show_pair
 
 def imshow(img, text=None, save=False):
     npimg = img.numpy()
@@ -25,16 +25,17 @@ def show_plot(iteration,loss):
     plt.plot(iteration,loss)
     plt.show()
 
+
 config = {'training_dir'     : './data/faces/training/',
           'testing_dir'      : './data/faces/evaluation/',
-          'train_batch_size' : 64,
+          'train_batch_size' : 128,
           'epoch'            : 100}
-
 
 samples = SiameseLoader()
 samples.load_data(config['training_dir'])
 samples.load_data(config['testing_dir'],'testing')
 
+data_samples = DataLoader('./data/faces')
 
 # Model
 def W_init(shape,name=None):
@@ -64,6 +65,8 @@ right_input = Input(input_shape)
 convnet = Sequential()
 convnet.add(Conv2D(64,(10,10),activation='relu',input_shape=input_shape,
                    kernel_initializer=W_init,kernel_regularizer=l2(2e-4)))
+convnet.add(Conv2D(64,(16,16),activation='relu',input_shape=input_shape,
+                   kernel_initializer=W_init,kernel_regularizer=l2(2e-4)))
 convnet.add(MaxPooling2D())
 convnet.add(Conv2D(128,(7,7),activation='relu',
                    kernel_regularizer=l2(2e-4),kernel_initializer=W_init,bias_initializer=b_init))
@@ -81,21 +84,41 @@ encoded_l = convnet(left_input)
 encoded_r = convnet(right_input)
 
 L1_distance = lambda x : K.abs(x[0]-x[1])
-distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([encoded_l,encoded_r])
-siamese_net = Model(input=[left_input,right_input],output=distance)
 
-optimizer = Adam(0.00006)
+# distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([encoded_l,encoded_r])
+# siamese_net = Model(input=[left_input,right_input],output=distance)
 
-siamese_net.compile(loss=contrastive_loss, optimizer=optimizer)
+# optimizer = Adam(lr=0.00006, decay=0.00001)
+
+both = merge([encoded_l,encoded_r], mode=L1_distance, output_shape=lambda x: x[0])
+prediction = Dense(1,activation='sigmoid',kernel_initializer=W_init,
+                   bias_initializer=b_init)(both)
+siamese_net = Model(input=[left_input,right_input],output=prediction)
+
+# siamese_net.compile(loss=contrastive_loss, optimizer=optimizer)
+
+siamese_net.compile(loss='binary_crossentropy',optimizer=optimizer)
 siamese_net.count_params()
-
 
 # Training
 for epoch in range(config['epoch']):
-    img, lbl = samples.random_pair()
-    # imgs, lbls = samples.get_batch(config['train_batch_size'])
-    target = 1 if lbl[0] == lbl[1] else 0
-    img0 = np.reshape(img[0],(112,92,1,1))
-    img1 = np.reshape(img[1],(112,92,1,1))
-    siamese_net.train_on_batch([img0,img1], target)
+    print("Epoch : {}".format(epoch))
+    inputs, targets = samples.batch(config['train_batch_size'])
+    inputs = np.array(inputs)
+    inputs = inputs.reshape(2,config['train_batch_size'],112,92)
+    in_left = inputs[0].reshape(config['train_batch_size'],112,92,1)
+    in_right = inputs[1].reshape(config['train_batch_size'],112,92,1)
+    inputs = [in_left, in_right]
+    targets = [int(e1==e2) for e1,e2 in targets]
+    print("-- Inputs : {}".format(len(inputs[0])))
+    loss = siamese_net.train_on_batch(inputs, targets)
+    print("-- Loss : {}".format(loss))
     
+    
+# for epoch in range(config['epoch']):
+#     print('Epoch : {}'.format(epoch))
+#     inputs, targets = data_samples.get_batch(8)
+#     print('-- Inputs : {}'.format(len(inputs[0])))
+#     loss = siamese_net.train_on_batch(inputs, targets)
+#     print('-- Loss : {}'.format(loss))
+
--- process_faces.py
+++ process_faces.py
@@ -13,11 +13,84 @@ save_path = '/home/rmn/sit/one-shot/data/faces'
 
 f_dict = {}
 
+def load_faces(path,n=0):
+    X = []
+    y = []
+    cat_dict = {}
+    faces_dict = {}
+    curr_y = n
+
+    for person_id in os.listdir(path):
+        print('Loading person ID: {}'.format(person_id))
+        faces_dict[person_id] = [curr_y,None]
+        person_path = os.path.join(path,person_id)
+        category_images = []
+
+        for person_image in os.listdir(person_path):
+            cat_dict[curr_y] = (person_id,person_image)
+            # category_images = []
+            image_path = os.path.join(person_path,person_image)
+            image = imread(image_path)
+            print('-- Image:{}'.format(person_image))
+            category_images.append(image)
+            y.append(curr_y)
+            print("len(category_images):{}".format(len(category_images)))
+        try:
+            X.append(np.stack(category_images))
+        except ValueError as e:
+            print(e)
+            print('Error -- Category Images:{}'.format(category_images))
+        curr_y += 1
+        faces_dict[person_id][1] = curr_y - 1
+
+    y = np.vstack(y)
+    X = np.stack(X)
+    return X, y, faces_dict
+
+def pickle_data(save_path,data='training'):
+    if data is 'training':
+        folder = train_folder
+    else:
+        folder = eval_folder
+    X,y,c = load_faces(folder)
+    with open(os.path.join(save_path,data+'.pickle'),'wb') as f:
+        pickle.dump((X,c),f)
+
 class DataLoader(object):
     def __init__(self,path,data_subsets=['training','evaluation']):
         self.data = {}
         self.categories = {}
         self.info = {}
+
+        for name in data_subsets:
+            file_path = os.path.join(path, name + '.pickle')
+            print('Loading data from {}'.format(file_path))
+            with open(file_path, 'rb') as f:
+                (X,c) = pickle.load(f)
+                self.data[name] = X
+                self.categories[name] = c
+
+    def get_batch(self,batch_size,s='training'):
+        X = self.data[s]
+        n_classes, n_examples, w, h = X.shape
+
+        categories = np.random.choice(n_classes, size=(batch_size,), replace=False)
+        pairs = [np.zeros((batch_size,w,h,1)) for i in range(2)]
+        targets = np.zeros((batch_size,))
+        targets[batch_size//2:] = 1
+        for i in range(batch_size):
+            category = categories[i]
+            idx_1 = np.random.randint(0,n_examples)
+            pairs[0][i,:,:,:] = X[category, idx_1].reshape(w,h,1)
+            idx_2 = np.random.randint(0,n_examples)
+
+            if i >= batch_size//2:
+                category_2 = category
+            else:
+                category_2 = (category + np.random.randint(1,n_classes)) % n_classes
+            pairs[1][i,:,:,:] = X[category_2,idx_2].reshape(w,h,1)
+        return pairs, targets
+
 
 class SiameseLoader(object):
     def __init__(self):
@@ -53,6 +126,22 @@ class SiameseLoader(object):
             imgs.append(random.choice(data[ps[1]]))
             cls = tuple(ps)
         return imgs, cls
+
+    def batch(self,batch_size,data_type='training'):
+        if data_type is 'training':
+            data = self.training_data
+        else:
+            data = self.testing_data
+
+        batch, labels = [], []
+        for i in range(batch_size):
+            if i > batch_size//2:
+                pair, cls = self.random_pair(2)
+            else:
+                pair, cls = self.random_pair(-1)
+            batch.append(pair)
+            labels.append(cls)
+        return batch, labels
 
     def get_batch(self,batch_size):
         data = self.training_data
